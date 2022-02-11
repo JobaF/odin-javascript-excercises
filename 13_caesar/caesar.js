2ba0e9b0b051e7f003fbeaaa93602c9ca7eceabf
Jabo Fecht <jifecht@gmail.com>
2022-02-11 17:50:26 +0100

    Add solution excercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,25 @@
-const caesar = function() {
-
-};
-
+const caesar = function (string, shift) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const positiveShift =
+    shift > 26 ? shift % 26 : shift < 0 ? 26 - (Math.abs(shift) % 26) : shift
+  return string
+    .split('')
+    .map((a) => {
+      if (alphabet.includes(a)) {
+        if (a.charCodeAt(0) <= 90) {
+          const shifted = a.charCodeAt(0) + positiveShift
+          return shifted > 90
+            ? String.fromCharCode(shifted - 26)
+            : String.fromCharCode(shifted)
+        } else {
+          const shifted = a.charCodeAt(0) + positiveShift
+          return shifted > 122
+            ? String.fromCharCode(shifted - 26)
+            : String.fromCharCode(shifted)
+        }
+      } else return a
+    })
+    .join('')
+}
 // Do not edit below this line
-module.exports = caesar;
+module.exports = caesar
