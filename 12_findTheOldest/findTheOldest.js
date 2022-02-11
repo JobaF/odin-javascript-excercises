8e327c79e26c334441b5823ef0dbcee6ab497664
Jabo Fecht <jifecht@gmail.com>
2022-02-11 17:26:04 +0100

    Add solution excercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function (people) {
+  return people.reduce((max, curr) => {
+    const deduction = curr.yearOfDeath || new Date().getFullYear()
+    const age = deduction - curr.yearOfBirth
+    const currElement = { ...curr, age }
+    return max.age > currElement.age ? max : currElement
+  }, 0)
+}
 // Do not edit below this line
-module.exports = findTheOldest;
+module.exports = findTheOldest
