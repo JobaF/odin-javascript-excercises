db38b9d096dd3f2f443d97f16a41d3cd8153d9ac
Jabo Fecht <jifecht@gmail.com>
2022-02-11 17:00:57 +0100

    Add solution excercise ten

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,10 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function (n) {
+  const num = parseInt(n)
+  if (num < 0) return 'OOPS'
+  else {
+    return num < 1 ? 0 : num <= 2 ? 1 : fibonacci(num - 1) + fibonacci(num - 2)
+  }
+}
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
