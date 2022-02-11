9530cdbb3972f56e2bafe9c5fa54135691f86658
Jabo Fecht <jifecht@gmail.com>
2022-02-11 16:09:53 +0100

    Add solution excercise eight

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,33 @@
-const add = function() {
-	
-};
+const add = function () {
+  return arguments[0] + arguments[1]
+}
 
-const subtract = function() {
-	
-};
+const subtract = function () {
+  return arguments[0] - arguments[1]
+}
 
-const sum = function() {
-	
-};
+const sum = function () {
+  return arguments[0].reduce((a, b) => a + b, 0)
+}
 
-const multiply = function() {
+const multiply = function () {
+  let result = 1
+  for (let i = 0; i < arguments[0].length; i++) {
+    result *= arguments[0][i]
+  }
+  return result
+}
 
-};
+const power = function () {
+  return Math.pow(arguments[0], arguments[1])
+}
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+let f = []
+const factorial = function (n) {
+  if (n == 0 || n == 1) return 1
+  if (f[n] > 0) return f[n]
+  return (f[n] = factorial(n - 1) * n)
+}
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +36,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
-};
+  factorial,
+}
