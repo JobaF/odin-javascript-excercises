0ffd8593a467b1a3a9ac956f4c711c697e05b567
Jabo Fecht <jifecht@gmail.com>
2022-02-11 15:08:18 +0100

    Add result second excercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = (string, repetitions) => {
+  if (parseInt(repetitions) < 0) return 'ERROR'
+  let result = ''
+  for (let i = 0; i < repetitions; i++) {
+    result += string
+  }
+  return result
+}
 
-};
+repeatString('hey', 3)
 
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString
