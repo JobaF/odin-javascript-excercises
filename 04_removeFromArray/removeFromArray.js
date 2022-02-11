319d2e434b594ad015f788f943ca3a85e8dfdcc5
Jabo Fecht <jifecht@gmail.com>
2022-02-11 15:23:13 +0100

    Add result excercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
-};
+function removeFromArray(array, value, secondValue = false) {
+  let arr = array
+  for (let i = 1; i <= arguments.length; i++) {
+    arr = arr.filter((v) => v !== arguments[i])
+  }
+  return arr
+}
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
