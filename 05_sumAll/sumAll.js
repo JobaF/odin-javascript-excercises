f356de3082b6582cf8dbc5f585ee2bcdba65c57c
Jabo Fecht <jifecht@gmail.com>
2022-02-11 15:42:25 +0100

    Add solution excercise five

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secondNum) {
+  if (typeof firstNum !== 'number' || typeof secondNum !== 'number') {
+    return 'ERROR'
+  } else if (firstNum < 0 || secondNum < 0) {
+    return 'ERROR'
+  } else {
+    let sum = 0
+    const bigger = firstNum > secondNum ? firstNum : secondNum
+    const smaller = firstNum < secondNum ? firstNum : secondNum
 
-};
-
+    for (let i = smaller; i <= bigger; i++) {
+      sum += i
+    }
+    return sum
+  }
+}
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
