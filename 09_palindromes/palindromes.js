7bf1ac36fff7a6e184db2275a21bd7e6001535af
Jabo Fecht <jifecht@gmail.com>
2022-02-11 16:48:16 +0100

    Add result excercise nine

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+  const regex = /[A-Za-z]+/gi
+  const match = string.match(regex).join('').toLowerCase()
+  return match === match.split('').reverse().join('')
+}
 
 // Do not edit below this line
-module.exports = palindromes;
+module.exports = palindromes
